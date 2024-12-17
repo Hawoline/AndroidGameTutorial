2538026b7b68677de82eb85993b4bbe2c56fbcf9
Hawoline <belikhawoline@gmail.com>
2024-12-17 23:51:54 +0700

    play with animation;

--- app/src/main/java/ru/hawoline/androidgametutorial/GamePanel.java
+++ app/src/main/java/ru/hawoline/androidgametutorial/GamePanel.java
@@ -17,21 +17,33 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
   private GameLoop gameLoop;
   private Random random = new Random();
   private float x, y;
+  private float yMoveDirection = 3;
   private ArrayList<PointF> skeletons = new ArrayList<>();
-  // Test GPG Key
+
+  private int playerFaceDirection, playerAnimationIndexY;
+  private int aniTick;
+  private int aniSpeed = 10;
 
   public GamePanel(Context context) {
     super(context);
     holder = getHolder();
     holder.addCallback(this);
     gameLoop = new GameLoop(this);
+    //for (int  i = 0; i < 50; i++) {
+    //  skeletons.add(new PointF(random.nextInt(1080), random.nextInt(540)));
+    //}
   }
 
   @Override public boolean onTouchEvent(MotionEvent event) {
     if (event.getAction() == MotionEvent.ACTION_DOWN) {
-      x = event.getX();
-      y = event.getY();
-      skeletons.add(new PointF(random.nextInt(1080), 0));
+      //x = event.getX();
+      //y = event.getY();
+      yMoveDirection = -yMoveDirection;
+      if (playerFaceDirection == 0) {
+        playerFaceDirection = 1;
+      } else {
+        playerFaceDirection = 0;
+      }
     }
     return true;
   }
@@ -49,6 +61,16 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
 
   }
 
+  public void render() {
+    Canvas canvas = holder.lockCanvas();
+    canvas.drawColor(Color.BLACK);
+    canvas.drawBitmap(GameCharacters.PLAYER.getSprite(playerAnimationIndexY, playerFaceDirection), x, y, null);
+    for (int i = 0; i < skeletons.size();i++) {
+      canvas.drawBitmap(GameCharacters.SKELETON.getSprite(0,0), skeletons.get(i).x, skeletons.get(i).y, null);
+    }
+    holder.unlockCanvasAndPost(canvas);
+  }
+
   public void update(double delta) {
     for (PointF skeletonPosition: skeletons) {
       skeletonPosition.y += delta * 300;
@@ -56,14 +78,23 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
         skeletonPosition.y = 0;
       }
     }
-  }
-  public void render() {
-    Canvas canvas = holder.lockCanvas();
-    canvas.drawColor(Color.BLACK);
-    canvas.drawBitmap(GameCharacters.PLAYER.getSprite(0, 0), x, y, null);
-    for (int i = 0; i < skeletons.size();i++) {
-      canvas.drawBitmap(GameCharacters.SKELETON.getSprite(0,0), skeletons.get(i).x, skeletons.get(i).y, null);
+    y += yMoveDirection;
+    if (y > 1980) {
+      y = 0;
     }
-    holder.unlockCanvasAndPost(canvas);
+    if (y < 0) {
+      y = 1980;
+    }
+    updateAnimation();
+  }
+  private void updateAnimation() {
+    aniTick++;
+    if (aniTick >= aniSpeed) {
+      aniTick = 0;
+      playerAnimationIndexY++;
+      if (playerAnimationIndexY > 4) {
+        playerAnimationIndexY = 0;
+      }
+    }
   }
 }
