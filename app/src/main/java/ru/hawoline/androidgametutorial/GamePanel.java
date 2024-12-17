d2a5f128e34ca14a9a9bed075fb7242bd1dc76f4
Hawoline <belikhawoline@gmail.com>
2024-12-18 00:37:36 +0700

    - 5 episode ended;

--- app/src/main/java/ru/hawoline/androidgametutorial/GamePanel.java
+++ app/src/main/java/ru/hawoline/androidgametutorial/GamePanel.java
@@ -11,6 +11,7 @@ import androidx.annotation.NonNull;
 import java.util.ArrayList;
 import java.util.Random;
 import ru.hawoline.androidgametutorial.entities.GameCharacters;
+import ru.hawoline.androidgametutorial.helpers.GameConstants;
 
 public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
   private SurfaceHolder holder;
@@ -18,9 +19,13 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
   private Random random = new Random();
   private float x, y;
   private float yMoveDirection = 3;
-  private ArrayList<PointF> skeletons = new ArrayList<>();
+  //private ArrayList<PointF> skeletons = new ArrayList<>();
+  private PointF skeletonPosition;
+  private int skeletonDirection = GameConstants.FaceDirection.DOWN;
+  private long lastSkeletonDirectionChange = System.currentTimeMillis();
 
-  private int playerFaceDirection, playerAnimationIndexY;
+  private int playerFaceDirection = GameConstants.FaceDirection.RIGHT;
+  private int playerAnimationIndexY;
   private int aniTick;
   private int aniSpeed = 10;
 
@@ -32,18 +37,31 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     //for (int  i = 0; i < 50; i++) {
     //  skeletons.add(new PointF(random.nextInt(1080), random.nextInt(540)));
     //}
+    skeletonPosition =  new PointF(random.nextInt(1080), random.nextInt(540));
   }
 
   @Override public boolean onTouchEvent(MotionEvent event) {
     if (event.getAction() == MotionEvent.ACTION_DOWN) {
-      //x = event.getX();
-      //y = event.getY();
-      yMoveDirection = -yMoveDirection;
-      if (playerFaceDirection == 0) {
-        playerFaceDirection = 1;
+      float newX = event.getX();
+      float newY = event.getY();
+
+      float xDifference = Math.abs(newX - x);
+      float yDifference = Math.abs(newY - y);
+      if (xDifference > yDifference) {
+        if (newX > x) {
+          playerFaceDirection = GameConstants.FaceDirection.RIGHT;
+        } else {
+          playerFaceDirection = GameConstants.FaceDirection.LEFT;
+        }
       } else {
-        playerFaceDirection = 0;
+        if (newY > y) {
+          playerFaceDirection = GameConstants.FaceDirection.DOWN;
+        } else {
+          playerFaceDirection = GameConstants.FaceDirection.UP;
+        }
       }
+      x = event.getX();
+      y = event.getY();
     }
     return true;
   }
@@ -65,25 +83,51 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     Canvas canvas = holder.lockCanvas();
     canvas.drawColor(Color.BLACK);
     canvas.drawBitmap(GameCharacters.PLAYER.getSprite(playerAnimationIndexY, playerFaceDirection), x, y, null);
-    for (int i = 0; i < skeletons.size();i++) {
-      canvas.drawBitmap(GameCharacters.SKELETON.getSprite(0,0), skeletons.get(i).x, skeletons.get(i).y, null);
-    }
+    //for (int i = 0; i < skeletons.size();i++) {
+    //  canvas.drawBitmap(GameCharacters.SKELETON.getSprite(0,0), skeletons.get(i).x, skeletons.get(i).y, null);
+    //}
+      canvas.drawBitmap(GameCharacters.SKELETON.getSprite(playerAnimationIndexY,skeletonDirection), skeletonPosition.x, skeletonPosition.y, null);
+
     holder.unlockCanvasAndPost(canvas);
   }
 
   public void update(double delta) {
-    for (PointF skeletonPosition: skeletons) {
-      skeletonPosition.y += delta * 300;
-      if (skeletonPosition.y > 1980) {
-        skeletonPosition.y = 0;
-      }
+    //for (PointF skeletonPosition: skeletons) {
+    //  skeletonPosition.y += delta * 300;
+    //  if (skeletonPosition.y > 1980) {
+    //    skeletonPosition.y = 0;
+    //  }
+    //}
+
+    if (System.currentTimeMillis() - lastSkeletonDirectionChange > 3000) {
+      skeletonDirection = random.nextInt(4);
+      lastSkeletonDirectionChange = System.currentTimeMillis();
     }
-    y += yMoveDirection;
-    if (y > 1980) {
-      y = 0;
-    }
-    if (y < 0) {
-      y = 1980;
+    switch (skeletonDirection) {
+      case GameConstants.FaceDirection.DOWN:
+        skeletonPosition.y += delta * 300;
+        if (skeletonPosition.y > 1980) {
+          skeletonDirection = GameConstants.FaceDirection.UP;
+        }
+        break;
+      case GameConstants.FaceDirection.UP:
+        skeletonPosition.y -= delta * 300;
+        if (skeletonPosition.y < 0) {
+          skeletonDirection = GameConstants.FaceDirection.DOWN;
+        }
+        break;
+      case GameConstants.FaceDirection.RIGHT:
+        skeletonPosition.x += delta * 300;
+        if (skeletonPosition.x > 1080) {
+          skeletonDirection = GameConstants.FaceDirection.LEFT;
+        }
+        break;
+      case GameConstants.FaceDirection.LEFT:
+        skeletonPosition.x -= delta * 300;
+        if (skeletonPosition.x < 0) {
+          skeletonDirection = GameConstants.FaceDirection.RIGHT;
+        }
+        break;
     }
     updateAnimation();
   }
@@ -92,7 +136,7 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     if (aniTick >= aniSpeed) {
       aniTick = 0;
       playerAnimationIndexY++;
-      if (playerAnimationIndexY > 4) {
+      if (playerAnimationIndexY > 3) {
         playerAnimationIndexY = 0;
       }
     }
---
+++ app/src/main/java/ru/hawoline/androidgametutorial/helpers/GameConstants.java
@@ -0,0 +1,10 @@
+package ru.hawoline.androidgametutorial.helpers;
+
+public final class GameConstants {
+  public static final class FaceDirection {
+    public static final int DOWN = 0;
+    public static final int UP = 1;
+    public static final int LEFT = 2;
+    public static final int RIGHT = 3;
+  }
+}
